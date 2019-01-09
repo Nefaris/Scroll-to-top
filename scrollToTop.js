bf241a5b1f12b0ddf6cf1e791550abd80bb4d9a7
Kamil <34476654+Nefaris@users.noreply.github.com>
2019-01-09 17:44:49 +0100

    First upload

---
+++ scrollToTop.js
@@ -0,0 +1,42 @@
+let stt_background_color = "#000";
+let stt_border_radius = "50%";
+let stt_right_pos = "50px";
+let stt_bottom_pos = "50px";
+let stt_width = "60px";
+let stt_height = "60px";
+
+let stt_icon = '<i class="fas fa-angle-up"></i>';
+let stt_icon_size = '1em';
+let stt_icon_color = "#fff";
+
+window.onload = () => {
+  document.head.innerHTML +=
+    ('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">' + '<style>html{scroll-behavior: smooth;}</style>');
+
+  let stt = document.createElement("div");
+  stt.innerHTML = stt_icon;
+  stt.style.cssText = 'cursor: pointer;' + 'position: fixed; ' + 'display: none;' + 'justify-content: center;' + 'align-items: center;' + 'width:' + stt_width + '; height: ' + stt_height + '; border-radius: ' + stt_border_radius + '; right: ' + stt_right_pos + '; bottom: ' + stt_bottom_pos + '; color: ' + stt_icon_color + '; background-color: ' + stt_background_color + '; font-size: ' + stt_icon_size;
+
+  document.body.appendChild(stt);
+
+  stt.addEventListener("click", () => {
+    document.body.scrollIntoView();
+  });
+
+  window.addEventListener('scroll', () => {
+    checkSize();
+  });
+
+  checkSize();
+
+  function checkSize() {
+    let scroll_h = window.scrollY;
+    let window_h = window.innerHeight;
+
+    if (scroll_h > window_h) {
+      stt.style.display = 'flex';
+    } else {
+      stt.style.display = 'none';
+    }
+  }
+};
